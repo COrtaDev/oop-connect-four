8d5908052352a39ba8113a6dd5755b6d56004371
Conrad Orta <conradorta@gmail.com>
2020-04-23 19:13:38 -0400

    Completed refactor in indicating current player

--- connect-four.js
+++ connect-four.js
@@ -1,15 +1,28 @@
 import { Game } from "./game.js";
 
 let game = undefined;
+let turns = 0;
 
 function updateUi() {
     const boardHolder = document.getElementById("board-holder");
     const gameName = document.getElementById("game-name");
+    const piece = document.createElement('div');
+
     if (game === undefined) {
         boardHolder.setAttribute('class', 'is-invisible');
     } else {
         gameName.innerHTML = game.getName();
         boardHolder.removeAttribute('class');
+        turns = game.turns;
+        game.currentPlayer = game.playInColumn();
+        if (game.currentPlayer === 1) {
+            piece.setAttribute('class', 'token black');
+            targets.setAttribute('class', 'red');
+        } else {
+            piece.setAttribute('class', 'token red');
+            targets.setAttribute('class', 'black');
+        }
+        board.appendChild(piece);
     }
 };
 
@@ -19,6 +32,7 @@ window.addEventListener('DOMContentLoaded', event => {
     const player2 = document.getElementById("player-2-name");
     const formHolder = document.getElementById("form-holder");
     const newGame = document.getElementById("new-game");
+    const targets = document.getElementById('click-targets');
 
     formHolder.addEventListener('keyup', event => {
         if (player1.value && player2.value) {
@@ -27,15 +41,15 @@ window.addEventListener('DOMContentLoaded', event => {
     })
 
     newGame.addEventListener('click', event => {
-        game = new Game(1, player1.value, player2.value);
+        game = new Game(1, player1.value, player2.value, 1);
         player1.value = '';
         player2.value = '';
         newGame.setAttribute('disabled', true);
         updateUi();
     })
 
-    const targets = document.getElementById('click-targets');
-    let turns = 0;
+    // const targets = document.getElementById('click-targets');
+    // let turns = game.turns;
     let columns = {
         column0: 5,
         column1: 5,
@@ -45,33 +59,29 @@ window.addEventListener('DOMContentLoaded', event => {
         column5: 5,
         column6: 5
     }
-    let currentPlayer = 'black';
-    targets.setAttribute('class', currentPlayer);
+    // let currentPlayer = 'black';
+    // targets.setAttribute('class', currentPlayer);
     let currentColumn = '';
     targets.addEventListener('click', event => {
         let column = event.target.id.replace('-', '');
         currentColumn = event.target.id[7];
-        if (columns[column] === -1) {
-            event.preventDefault();
-            return;
-        }
 
-        if (turns % 2 !== 0) {
-            currentPlayer = 'red';
-        } else {
-            currentPlayer = 'black';
-        }
-        console.log(turns);
-        console.log(currentColumn);
+        // if (turns % 2 !== 0) {
+        //     currentPlayer = 'red';
+        // } else {
+        //     currentPlayer = 'black';
+        // }
+        // console.log(turns);
+        // console.log(currentColumn);
 
 
-        console.log(column); //'column-0' --> .join('-')
+        // console.log(column); //'column-0' --> .join('-')
         let row = columns[column];
-        console.log(row);
+        // console.log(row);
         let id = `square-${row}-${currentColumn}`;
 
-        const piece = document.createElement('div');
-        piece.setAttribute('class', `token ${currentPlayer}`);
+        // const piece = document.createElement('div');
+        // piece.setAttribute('class', `token ${currentPlayer}`);
 
         let board = document.getElementById(id);
 
@@ -80,12 +90,17 @@ window.addEventListener('DOMContentLoaded', event => {
         //handleRow();
 
         turns++;
+        // playInColumn()
         if (turns % 2 !== 0) {
             currentPlayer = 'red';
         } else {
             currentPlayer = 'black';
         }
         targets.setAttribute('class', currentPlayer);
+
+        if (columns[column] === -1) {
+            event.target.setAttribute('class', 'click-target full');
+        }
     })
 
 
--- game.js
+++ game.js
@@ -1,11 +1,19 @@
 export class Game {
-    constructor(turns, player1, player2, ) {
+    constructor(turns, player1, player2, currentPlayer) {
         this.turns = turns;
         this.player1 = player1;
         this.player2 = player2;
+        this.currentPlayer = currentPlayer;
 
     }
     getName() {
         return `${this.player1} vs ${this.player2}`;
+    }
+    playInColumn() {
+        if (this.currentPlayer === 1) {
+            return 2;
+        } else {
+            return 1;
+        }
     }
 }
